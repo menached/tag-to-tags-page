adbeda962c9bbd8c2dc7e7ac41b7343e85e37db2
David Menache <dave@doap.com>
2023-01-14 08:57:49 -0600

    Update index.php
    Change functionality. Now redirects tag links to do a search for that tag.

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php
 /*
 Plugin Name: Redirect Tag Pages
-Plugin URI: http://yourdomain.com/
-Description: Redirects all pages marked with a particular tag to an alternate Wordpress page named Tags
+Plugin URI: http://devopsandplatforms.com/
+Description: Redirects tag links to do a search for that tag.
 Version: 1.0
 Author: David Menache
 */
 
-add_action( 'template_redirect', 'redirect_tag_pages' );
-
-function redirect_tag_pages() {
-    if ( is_tag() ) {
-        wp_redirect( home_url( 'tags' ) );
-        exit;
-    }
+function wp_redirect_tag_to_home() {
+	global $wp_query;
+	if ( is_tag() ) {
+		$tag = get_queried_object()->name;
+		wp_redirect( home_url('/?s='.$tag), 301 );
+		exit;
+	}
 }
+add_action('template_redirect', 'wp_redirect_tag_to_home');
 ?>
